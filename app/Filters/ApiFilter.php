ee74b745734876afca1cedfbaaf1418fdcedeb97
KevinWesselink <79516416+KevinWesselink@users.noreply.github.com>
2023-06-02 12:28:28 +0200

    Filtering more data
    Hfdstk3les4.

--- app/Filters/ApiFilter.php
+++ app/Filters/ApiFilter.php
@@ -1,32 +1,16 @@
 <?php 
 
-namespace App\Services\V1;
+namespace App\Filters;
 
 use Illuminate\Http\Request;
 
 
-Class CustomerQuery {
-    protected $safeParms = [
-        'name' => ['eq'],
-        'type' => ['eq'],
-        'email' => ['eq'],
-        'address' => ['eq'],
-        'city' => ['eq'],
-        'state' => ['eq'],
-        'postalCode' => ['eq', 'gt', 'lt']
-    ];
+Class ApiFilter {
+    protected $safeParms = [];
 
-    protected $columnMap = [
-        'postalCode' => 'postal_code'
-    ];
+    protected $columnMap = [];
 
-    protected $operatorMap = [
-        'eq' => '=',
-        'lt' => '<',
-        'lte' => '<=',
-        'gt' => '>',
-        'gte' => '>='
-    ];
+    protected $operatorMap = [];
 
     public function transform(Request $request) {
         $eloQuery = [];
