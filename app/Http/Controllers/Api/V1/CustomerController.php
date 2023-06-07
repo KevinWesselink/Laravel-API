3e40e8775f903d9686f639e07fd9fee09900e487
KevinWesselink <79516416+KevinWesselink@users.noreply.github.com>
2023-06-07 14:32:12 +0200

    Updating with PUT and PATCH
    Hfdstk4les2

--- app/Http/Controllers/Api/V1/CustomerController.php
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -5,9 +5,9 @@ namespace App\Http\Controllers\Api\V1;
 use App\Models\Customer;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\StoreCustomerRequest;
+use App\Http\Requests\V1\StoreCustomerRequest;
 use App\Http\Resources\V1\CustomerResource;
-use App\Http\Requests\UpdateCustomerRequest;
+use App\Http\Requests\V1\UpdateCustomerRequest;
 use App\Http\Resources\V1\CustomerCollection;
 use App\Filters\V1\CustomerFilter;
 
@@ -59,7 +59,7 @@ class CustomerController extends Controller
      */
     public function update(UpdateCustomerRequest $request, Customer $customer)
     {
-        //
+        $customer->update($request->all());
     }
 
     /**
